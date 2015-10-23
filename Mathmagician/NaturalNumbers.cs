2a110292ca82713403227eafefa2b509120db7a0
Taylor Miley <coachtaylormiley@gmail.com>
2015-10-23 14:31:26 -0500

    Completed natural numbers implementation with tests

---
+++ Mathmagician/NaturalNumbers.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace Mathmagician
+{
+    public class NaturalNumbers
+    {
+        private int first;
+
+        public NaturalNumbers()
+        {
+            first = 1;
+        }
+
+        public int GetNext(int x)
+        {
+            return x+1;
+        }
+
+        public int GetFirst()
+        {
+            return first;
+        }
+
+        public int[] GetSequence()
+        {
+            return GetSequence(10);
+        }
+
+        public int[] GetSequence(int length)
+        {
+            List<int> output = new List<int>();
+            output.Add(GetFirst());
+            while (output.Count < length)
+            {
+                output.Add(GetNext(output.Last()));
+            }
+            return output.ToArray();
+        }
+
+        public string ToString(int[] array)
+        {
+            /*
+            string output = "";
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] == array.Last())
+                {
+                    output += array[i].ToString();
+                } else
+                {
+                    output += array[i].ToString() + " ";
+                }
+                
+                //output += array[i].ToString() + " ";
+            }*/
+            string output = String.Join(" ", array);
+            return output;
+        }
+    }
+}
