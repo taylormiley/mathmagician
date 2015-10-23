a4360d817399f4649acb6f67463c01c77b69133b
Taylor Miley <coachtaylormiley@gmail.com>
2015-10-23 15:55:00 -0500

    Initial commit for classes and tests for Odd, Prime, and Fibonacci

---
+++ MathmagicianTests/FibonacciTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MathmagicianTests
+{
+    [TestClass]
+    public class FibonacciTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
