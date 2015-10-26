41522e81d82ce374436287a2b6af75dec4dd076c
Taylor Miley <coachtaylormiley@gmail.com>
2015-10-26 11:34:06 -0500

    added user interaction funcionality with all number types, also finished touch ups on prime numbers

--- Mathmagician/Program.cs
+++ Mathmagician/Program.cs
@@ -32,6 +32,73 @@ namespace Mathmagician
                 }
                 
             }
+            if (response == "even numbers") {
+                Console.WriteLine("How many?");
+                Console.Write(prompt);
+                string response_length = Console.ReadLine();
+                int length;
+                bool parsed = int.TryParse(response_length, out length);
+                if (parsed)
+                {
+                    EvenNumbers even = new EvenNumbers();
+                    Console.WriteLine(even.ToString(even.GetSequence(length)));
+                }
+                else
+                {
+                    Console.WriteLine("Whoops!");
+                }
+            }
+            if (response == "odd numbers")
+            {
+                Console.WriteLine("How many?");
+                Console.Write(prompt);
+                string response_length = Console.ReadLine();
+                int length;
+                bool parsed = int.TryParse(response_length, out length);
+                if (parsed)
+                {
+                    OddNumbers odd = new OddNumbers();
+                    Console.WriteLine(odd.ToString(odd.GetSequence(length)));
+                }
+                else
+                {
+                    Console.WriteLine("Whoops!");
+                }
+            }
+            if (response == "fibonacci numbers")
+            {
+                Console.WriteLine("How many?");
+                Console.Write(prompt);
+                string response_length = Console.ReadLine();
+                int length;
+                bool parsed = int.TryParse(response_length, out length);
+                if (parsed)
+                {
+                    FibonacciNumbers fibonacci = new FibonacciNumbers();
+                    Console.WriteLine(fibonacci.ToString(fibonacci.GetSequence(length)));
+                }
+                else
+                {
+                    Console.WriteLine("Whoops!");
+                }
+            }
+            if (response == "prime numbers")
+            {
+                Console.WriteLine("How many?");
+                Console.Write(prompt);
+                string response_length = Console.ReadLine();
+                int length;
+                bool parsed = int.TryParse(response_length, out length);
+                if (parsed)
+                {
+                    PrimeNumbers prime = new PrimeNumbers();
+                    Console.WriteLine(prime.ToString(prime.GetSequence(length)));
+                }
+                else
+                {
+                    Console.WriteLine("Whoops!");
+                }
+            }
             else
             {
                 Console.WriteLine("Nope! Do better next time.");
