9419af6b76e0cead87838cfa85224f34995b0e61
Taylor Miley <coachtaylormiley@gmail.com>
2015-10-26 10:26:43 -0500

    completed prime unit testing with a lot of help from brendans code

--- Mathmagician/PrimeNumbers.cs
+++ Mathmagician/PrimeNumbers.cs
@@ -6,7 +6,50 @@ using System.Threading.Tasks;
 
 namespace Mathmagician
 {
-    class PrimeNumbers
+    public class PrimeNumbers : NaturalNumbers
     {
+        public PrimeNumbers()
+        {
+            first = 2;
+            increment = 2;
+        }
+
+        public bool isPrime(int x)
+        {
+            for (int i = 2; i < (x / 2); i++)
+            {
+                if (x % i == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        new public int[] GetSequence(int length)
+        {
+            List<int> output = new List<int>();
+            output.Add(GetFirst());
+
+            if (output.Count < length)
+            {
+                output.Add(3);
+            }
+
+            int counter = 5;
+            while (output.Count < length)
+            {
+                if (isPrime(counter))
+                {
+                    output.Add(counter);
+                    counter += 2;
+                }
+                else
+                {
+                    counter += 2;
+                }
+            }
+            return output.ToArray();
+        }
     }
 }
--- MathmagicianTests/PrimeTests.cs
+++ MathmagicianTests/PrimeTests.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Mathmagician;
 
 namespace MathmagicianTests
 {
@@ -7,8 +8,49 @@ namespace MathmagicianTests
     public class PrimeTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void PrimeEnsureICanCreateClassInstance()
         {
+            PrimeNumbers prime = new PrimeNumbers();
+            Assert.IsNotNull(prime);
+        }
+
+        [TestMethod]
+        public void PrimeShouldPrintFirstNumber()
+        {
+            PrimeNumbers prime = new PrimeNumbers();
+            int expected = 2;
+            int actual = prime.GetFirst();
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void PrimeShouldPrintNextNumber()
+        {
+            PrimeNumbers prime = new PrimeNumbers();
+            int input = 2;
+            int expected = 4;
+            int actual = prime.GetNext(input);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void PrimeShouldPrintAVariableSequenceOfNumbers()
+        {
+            PrimeNumbers prime = new PrimeNumbers();
+            int input = 8;
+            int[] expected = new int[] { 2, 3, 5, 7, 11, 13, 17, 19 };
+            int[] actual = prime.GetSequence(input);
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void PrimePrintSequenceOfNumbers()
+        {
+            PrimeNumbers prime = new PrimeNumbers();
+            int length = 7;
+            string expected = "2 3 5 7 11 13 17";
+            string actual = prime.ToString(prime.GetSequence(length));
+            Assert.AreEqual(expected, actual);
         }
     }
 }
