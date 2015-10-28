25436c44a8be82a77f7edef40728d960ba336838
Taylor Miley <coachtaylormiley@gmail.com>
2015-10-28 15:45:38 -0500

    refactored primes while following along with class

--- MathmagicianTests/PrimeTests.cs
+++ MathmagicianTests/PrimeTests.cs
@@ -28,7 +28,7 @@ namespace MathmagicianTests
         {
             PrimeNumbers prime = new PrimeNumbers();
             int input = 2;
-            int expected = 4;
+            int expected = 3;
             int actual = prime.GetNext(input);
             Assert.AreEqual(expected, actual);
         }
@@ -50,6 +50,43 @@ namespace MathmagicianTests
             int length = 7;
             string expected = "2 3 5 7 11 13 17";
             string actual = prime.ToString(prime.GetSequence(length));
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void PrimeFindIndexOfElement()
+        {
+            PrimeNumbers prime = new PrimeNumbers();
+            int input = 73;
+            int expected = 20;
+            int actual = prime.FindIndex(input);
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void PrimeFindIndexOfElementFails()
+        {
+            PrimeNumbers prime = new PrimeNumbers();
+            int input = 103;
+            prime.FindIndex(input);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void PrimeGetNextNonPrime()
+        {
+            PrimeNumbers prime = new PrimeNumbers();
+            int input = 105;
+            prime.GetNext(input);
+        }
+
+        [TestMethod]
+        public void PrimeIsPrime()
+        {
+            PrimeNumbers prime = new PrimeNumbers();
+            int input = 20;
+            bool expected = false;
+            bool actual = prime.IsPrime(input);
             Assert.AreEqual(expected, actual);
         }
     }
